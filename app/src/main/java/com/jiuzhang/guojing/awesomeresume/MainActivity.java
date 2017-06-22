e01ca20bafc18990792bb89b6203c47846f0ac19
linyujie <sheldonlinyujie@gmail.com>
2017-06-21 17:31:14 -0700

    TODO finished

--- app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
+++ app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
@@ -41,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         // TODO 3: Display the education data onto the UI
         // Follow the example in setupBasicInfoUI
         // You will probably find formatItems method useful when displaying the courses
+        ((TextView) findViewById(R.id.education_school)).setText(education.school + "(" + DateUtils.dateToString(education.startDate) + " ~ " + DateUtils.dateToString(education.endDate) + ")");
+        ((TextView) findViewById(R.id.education_courses)).setText(formatItems(education.courses));
     }
 
     private void fakeData() {
@@ -56,9 +58,13 @@ public class MainActivity extends AppCompatActivity {
         // TODO 1: Set the endDate
         // Follow the above example for startDate
         // DateUtils is a class written by ourselves, check out util/DateUtils
-
+        education.endDate = DateUtils.stringToDate("12/2015");
         // TODO 2: Add some fake courses in education1.courses
         education.courses = new ArrayList<>();
+        education.courses.add("Database");
+        education.courses.add("Algorithm");
+        education.courses.add("Networks");
+
     }
 
     public static String formatItems(List<String> items) {
